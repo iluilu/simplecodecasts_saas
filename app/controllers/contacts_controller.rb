ddd34251150d62ba64b503e42e1b088f7fdd16ab
Ilkka Kangas <ilkka.kang45@gmail.com>
2015-07-24 05:43:30 +0000

    Added device gem for users

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,7 +13,6 @@ class ContactsController < ApplicationController
             
             ContactMailer.contact_email(name, email, body).deliver
             
-            
             flash[:success] = 'Message sent.'
             redirect_to new_contact_path
         else
